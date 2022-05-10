517c13dfceeb4b81d46d58eab96f90d0b215bad2
Kaitlin Weber <weberk@rivm-biohn-l05p.int.ssc-campus.nl>
2022-05-10 17:40:42 +0200

    updated script

--- bin/exid16s.py
+++ bin/exid16s.py
@@ -12,23 +12,25 @@ def get_sample_name(file_path):
     sample_ID = str(file_path.stem)
     return sample_ID
 
-def extract_16S_Barrnap(list_of_fasta_files, barrnap_out):
-    ''' Runs barrnap and extracts the 16S rDNA sequence from Barrnap results
-    ''' 
-    list_barrnap = []
+def run_barrnap(list_of_fasta_files, barrnap_out):
+    ''' Runs barrnap with fasta files to extract rDNA
+    '''  
     for fasta_file in list_of_fasta_files:
         sample_name = get_sample_name(fasta_file)
         barrnap_dir = "barrnap_result/"
         for parent_dir in barrnap_out:
             path = os.path.join(parent_dir, barrnap_dir)
-            print(path)
             try:
                 os.mkdir(path)
             except FileExistsError:
                 pass
-        subprocess.call(["barrnap", fasta_file, "--outseq", path + f"{sample_name}.fasta"])
-        
-    
+        #subprocess.call(["barrnap", fasta_file, "--outseq", path + f"{sample_name}.fasta"])
+        return parent_dir
+
+def extract_16S_barrnap(path):
+    ''' Extracts the 16S sequence from the barrnap results 
+    ''' 
+    list_barrnap = []
     for root, dirs, all_files in os.walk(path):
         for data_files in all_files:
             files = os.path.join(root, data_files)
@@ -37,10 +39,35 @@ def extract_16S_Barrnap(list_of_fasta_files, barrnap_out):
     record_list = extraction_16Ssequence(list_barrnap)
 
     sequence_16S_dir = "FASTA_16S_sequence"
-    output_path = os.path.join(parent_dir, sequence_16S_dir)
+    output_path = os.path.join(path, sequence_16S_dir)
+
     create_output_file(record_list, list_barrnap, output_path)
+    return output_path
 
 
+def run_Kraken2(parent_dir, path_16s, database):
+    ''' Uses 16S sequence from barrnap to run Kraken2
+    '''
+    # Get fasta files with 16S sequence 
+    for root, dirs, all_files in os.walk(path_16s):
+        for data_files in all_files:
+            files = os.path.join(root, data_files)
+    
+    #database is posixpath 
+  
+    kraken_folder = "Kraken2_kreports"
+    kraken_output_folder = os.path.join(parent_dir, kraken_folder)
+    try:
+        os.mkdir(kraken_output_folder)
+    except FileExistsError:
+        pass
+    print(kraken_output_folder)
+
+
+
+    
+
+    
 
 
 
@@ -48,15 +75,15 @@ def extract_16S_Barrnap(list_of_fasta_files, barrnap_out):
 if __name__ == '__main__':
     argument_parser = argparse.ArgumentParser()
     argument_parser.add_argument('-i', '--input', type=pathlib.Path, 
-                        default=[], nargs='+', help='Filepath to assembled input files')
+                        default=[], nargs='+', help='All input files, this would be the assembled WGS data')
     argument_parser.add_argument('-o', '--output', type=pathlib.Path, 
                         default=[], nargs='+', help='Filepath to output directory')
     argument_parser.add_argument('-db', '--database', type=pathlib.Path, 
-                        default=[], nargs='+', help='Path to folder with databases which need to be combined')
+                        default=[], nargs='+', help='Path to folder with database')
     argument_parser.add_argument('-e', '--email', type=str, help='Enter email from NCBI account')
     args = argument_parser.parse_args()
-    extract_16S = extract_16S_Barrnap(list_of_fasta_files=args.input, barrnap_out=args.output)
-
-
-
+    barrnap_result= run_barrnap(list_of_fasta_files=args.input, barrnap_out=args.output)
+    sequence_16s = extract_16S_barrnap(path=barrnap_result)
+    run_Kraken2(parent_dir=barrnap_result, path_16s=sequence_16s, database=args.database)
     
+
